f7af1ecaefc29ce752294d46261bd803279cb2f3
Reilly Spitzfaden (they/them) <reillypascal@gmail.com>
2022-06-25 18:54:01 -0400

    Add files via upload

--- reillyspitzfaden/styles/scripts.js
+++ reillyspitzfaden/styles/scripts.js
@@ -19,22 +19,3 @@ function hamburgerOnOff() {
     x.style.display = "none";
     }
 }
-
-// ".content" works, but "a" doesn't seem to register right
-/*
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if ((event.target.matches(".content")) && window.innerWidth < 640) {
-      var dropdowns = document.getElementById("menuLinks");
-      dropdowns.style.display = "none";
-    }
-}
-// Touchscreen version of same
-window.ontouchstart = function(event) {
-    console.log(event.target);
-    if (!event.target.matches(".nav", "a", ".fa-bars") && window.innerWidth < 640) {
-      var dropdowns = document.getElementById("menuLinks");
-      dropdowns.style.display = "none";
-    }
-}
-*/
