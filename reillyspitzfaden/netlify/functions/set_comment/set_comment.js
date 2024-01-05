9c445165b8a3debb0c40cb5ddc06d13467b7065d
Reilly Spitzfaden <reillypascal@gmail.com>
2024-01-05 01:42:58 -0500

    getting 200 response code, supabase db name corrected

--- reillyspitzfaden/netlify/functions/set_comment/set_comment.js
+++ reillyspitzfaden/netlify/functions/set_comment/set_comment.js
@@ -9,13 +9,23 @@ const { createClient } = require('@supabase/supabase-js');
 // const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 const supabase = createClient('https://abdrchiaagsnvanffqjl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZHJjaGlhYWdzbnZhbmZmcWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzMTQwMDgsImV4cCI6MjAxOTg5MDAwOH0.3_8ocSvmJN6LWnibcNEhpv_zhVHwt5YS4srW12nXypc');
 
-exports.handler = async event => {
+exports.handler = async (event, context, callback) => {
+    // let payload = JSON.parse(event.body).payload;
     const { data, error } = await supabase
-        .from('blog_comments')
+        .from('comments')
         .insert([
-            { comment: 'Test comment'},
+            {   id: 5,
+                name: "Reilly Spitzfaden",
+                email: "reillypascal@gmail.com",
+                postURL: "http://localhost:8888/blogposts/01-03-2024.html",
+                comment: 'Test comment',
+                created_at: '2024-01-03 00:00:00+00',
+                show: 'true'
+            }
         ]);
 
-    console.log(data, error);
-    // console.log("set_comment called");
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    };
 }
