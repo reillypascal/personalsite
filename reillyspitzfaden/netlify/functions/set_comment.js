eec7a878d3c36a9f5833a6f947ab604ee4c00091
Reilly Spitzfaden <reillypascal@gmail.com>
2024-01-04 20:25:14 -0500

    added index.js

--- reillyspitzfaden/netlify/functions/set_comment.js
+++ reillyspitzfaden/netlify/functions/set_comment.js
@@ -4,7 +4,6 @@ const {
     SUPABASE_SERVICE_API_KEY
 } = process.env;
 
-const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
 exports.handler = async event => {
