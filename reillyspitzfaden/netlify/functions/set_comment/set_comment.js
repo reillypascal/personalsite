b7a85a7db1116c99b5e3e3c5a50321f7929cc8e2
Reilly Spitzfaden <reillypascal@gmail.com>
2024-01-05 19:14:50 -0500

    fixed 500 error from set_comment

--- reillyspitzfaden/netlify/functions/set_comment/set_comment.js
+++ reillyspitzfaden/netlify/functions/set_comment/set_comment.js
@@ -10,7 +10,9 @@ const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient('https://abdrchiaagsnvanffqjl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZHJjaGlhYWdzbnZhbmZmcWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzMTQwMDgsImV4cCI6MjAxOTg5MDAwOH0.3_8ocSvmJN6LWnibcNEhpv_zhVHwt5YS4srW12nXypc');
 
 exports.handler = async (event, context, callback) => {
-    const { data, error } = await supabase
+    let data
+    try {
+        data = await supabase
         .from('comments')
         .insert([
             {   
@@ -23,16 +25,16 @@ exports.handler = async (event, context, callback) => {
             }
         ])
         .select()
-        .then(() => {
-            return {
-                statusCode: 200,
-                body: "success"
-            };
-        })
-        .catch(() => {
-            return {
-                statusCode: 500,
-                body: "error"
-            };
-        });
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+              error: e.message
+            })
+          }
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    };
 }
